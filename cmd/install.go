175a4aed0c12c5bcb306ebfe3911949d366cd427
ren_jw <ren_jw>
2020-10-02 14:18:23 +0800

    添加redis单点离线安装命令

--- cmd/install.go
+++ cmd/install.go
@@ -6,31 +6,38 @@ import (
 	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
+	"os"
+	"strings"
 )
 
 const RedisConfigPath = "/etc/redis.conf"
 
 var installValidArgs = []string{"docker", "nginx", "redis"}
 var (
-	Offline       bool
-	RedisPort     string
-	RedisBindIP   string
-	RedisPassword string
-	RedisDataDir  string
-	RedisLogDir   string
-	netConnectErr error
-	installErr    error
+	offline         bool
+	redisPort       string
+	redisBindIP     string
+	redisPassword   string
+	redisDataDir    string
+	redisLogDir     string
+	redisBinaryPath string
+	filePath        string
+	netConnectErr   error
+	installErr      error
 )
 
 func init() {
 	netConnectErr = errors.New("网络连接异常,请选择离线方式安装...")
 	installErr = errors.New("程序安装失败...")
-	installRedisCmd.Flags().BoolVarP(&Offline, "offline", "o", false, "offline mode")
-	installRedisCmd.Flags().StringVarP(&RedisPort, "port", "p", "6379", "Redis listen port")
-	installRedisCmd.Flags().StringVarP(&RedisBindIP, "bind", "b", "0.0.0.0", "Redis bind address")
-	installRedisCmd.Flags().StringVarP(&RedisPassword, "password", "a", "redis", "Redis password")
-	installRedisCmd.Flags().StringVarP(&RedisDataDir, "data", "d", "/var/lib/redis", "Redis persistent directory")
-	installRedisCmd.Flags().StringVarP(&RedisDataDir, "log-file", "l", "/var/log/redis", "Redis logfile directory")
+	installRedisCmd.Flags().BoolVarP(&offline, "offline", "o", false, "offline mode")
+	installRedisCmd.Flags().StringVarP(&redisPort, "port", "p", "6379", "Redis listen port")
+	installRedisCmd.Flags().StringVarP(&redisBindIP, "bind", "b", "0.0.0.0", "Redis bind address")
+	installRedisCmd.Flags().StringVarP(&redisPassword, "password", "a", "redis", "Redis password")
+	installRedisCmd.Flags().StringVarP(&redisDataDir, "data", "d", "/var/lib/redis", "Redis persistent directory")
+	installRedisCmd.Flags().StringVarP(&redisLogDir, "log-file", "", "/var/log/redis", "Redis logfile directory")
+	installRedisCmd.Flags().StringVarP(&filePath, "file", "f", "", "redis-x-x-x.tar.gz path")
+	installRedisCmd.Flags().StringVarP(&redisBinaryPath, "binary-path", "", "/usr/bin/", "redis-* binary file path")
+
 	installCmd.AddCommand(installDockerCmd)
 	installCmd.AddCommand(installNginxCmd)
 	installCmd.AddCommand(installRedisCmd)
@@ -54,7 +61,7 @@ var installDockerCmd = &cobra.Command{
 	Use:   "docker [flags]",
 	Short: "install docker through easyctl",
 	Example: "\neasyctl install docker 在线安装docker" +
-		"\neasyctl install docker --offline=true --file=./v19.03.13.tar.gz 离线安装docker",
+		"\neasyctl install docker --offline --file=./v19.03.13.tar.gz 离线安装docker",
 	Run: func(cmd *cobra.Command, args []string) {
 		installDocker()
 	},
@@ -78,8 +85,10 @@ var installRedisCmd = &cobra.Command{
 	Example: "\neasyctl install redis 在线安装redis" +
 		"\neasyctl install redis --offline=true --file=./redis-5.0.5.tar.gz 离线安装redis",
 	Run: func(cmd *cobra.Command, args []string) {
-		if !Offline {
+		if !offline {
 			installRedisOnline()
+		} else {
+			installRedisOffline()
 		}
 	},
 }
@@ -154,25 +163,59 @@ func installRedisOnline() {
 	}
 
 	configRedis()
-	fmt.Println("[redis]启动redis...")
-	startRe, _ := util.ExecuteCmd(sys.SystemInfoObject.ServiceAction.StartRedis)
-	fmt.Println("[redis]配置redis为系统服务...")
-	enableRe, _ := util.ExecuteCmd(sys.SystemInfoObject.ServiceAction.StartRedisForever)
-	if startRe == nil && enableRe == nil {
-		util.PrintSuccessfulMsg("redis安装成功...")
+	startRedis()
+}
+
+// 在线安装redis
+func installRedisOffline() {
+
+	gcc := "rpm -qa|grep \"^gcc\";echo $?"
+	gccRe, _ := util.ExecuteCmdAcceptResult(gcc)
+	if gccRe != "0" {
+		installGcc, _ := util.ExecuteCmdAcceptResult("yum install -y gcc;echo $?")
+		if installGcc != "0" {
+			panic(errors.New("安装gcc失败，请配置好yum源..."))
+		}
 	}
+
+	_, err := os.Stat(filePath)
+	if err != nil {
+		panic(errors.New("访问redis源码包失败"))
+	}
+
+	compileRedis()
+	startRedis()
+}
+
+func compileRedis() {
+	fmt.Println("[redis]编译redis...")
+	directoryName := strings.Trim(strings.Trim(filePath, ".tar.gz"), "/")
+	util.ExecuteCmdAcceptResult("tar zxvf " + filePath)
+	util.ExecuteCmdAcceptResult(fmt.Sprintf("cd %s;make;make install;cp redis.conf %s;cd -", directoryName, RedisConfigPath))
+	util.ExecuteCmd(fmt.Sprintf("cp /usr/local/bin/redis-* %s", redisBinaryPath))
+
+	// 创建redis用户
+	fmt.Println("[redis]创建redis用户...")
+	sys.AddUser("redis", "", false)
+
+	fmt.Println("[redis]配置redis...")
+	configRedis()
+	// 配置系统服务
+	fmt.Println("[redis]配置redis系统服务...")
+	sys.ConfigService("redis")
+
 }
 
 // 配置redis
 func configRedis() {
+	// todo 校验参数合法性
 	modifyBgsaveErr := fmt.Sprintf("sed -i \"s#stop-writes-on-bgsave-error yes#stop-writes-on-bgsave-error no#g\" %s", RedisConfigPath)
 	modifyDaemonCmd := fmt.Sprintf("sed -i 's#daemonize no#daemonize yes#g' %s", RedisConfigPath)
-	modifyBindCmd := fmt.Sprintf("sed -i \"s#port 6379#port %s#g\" %s", RedisPort, RedisConfigPath)
-	modifyListenPortCmd := fmt.Sprintf("sed -i \"s#bind 127.0.0.1#bind %s#g\" %s", RedisBindIP, RedisConfigPath)
-	modifyPassword := fmt.Sprintf("echo \"requirepass %s\" >> %s", RedisPassword, RedisConfigPath)
-	modifyDataDir := fmt.Sprintf("sed -i \"s#dir ./#dir %s#g\" %s", RedisDataDir, RedisConfigPath)
-	modifyLogDir := fmt.Sprintf("sed -i \"s#logfile \\\"\\\"#logfile %s/redis.log#g\" %s", RedisLogDir, RedisConfigPath)
-	fmt.Println("[redis]配置redis...")
+	modifyBindCmd := fmt.Sprintf("sed -i \"s#port 6379#port %s#g\" %s", redisPort, RedisConfigPath)
+	modifyListenPortCmd := fmt.Sprintf("sed -i \"s#bind 127.0.0.1#bind %s#g\" %s", redisBindIP, RedisConfigPath)
+	modifyPassword := fmt.Sprintf("echo \"requirepass %s\" >> %s", redisPassword, RedisConfigPath)
+	modifyDataDir := fmt.Sprintf("sed -i \"s#dir ./#dir %s#g\" %s", redisDataDir, RedisConfigPath)
+	modifyLogDir := fmt.Sprintf("sed -i \"s#logfile \\\"\\\"#logfile %s/redis.log#g\" %s", redisLogDir, RedisConfigPath)
 	util.ExecuteCmd(modifyBgsaveErr)
 	util.ExecuteCmd(modifyDaemonCmd)
 	util.ExecuteCmd(modifyBindCmd)
@@ -181,7 +224,17 @@ func configRedis() {
 	util.ExecuteCmd(modifyDataDir)
 	util.ExecuteCmd(modifyLogDir)
 	fmt.Println("[redis]创建redis工作目录...")
-	util.ExecuteCmd(fmt.Sprintf("mkdir -p %s;mkdir -p %s", RedisLogDir, RedisDataDir))
+	util.ExecuteCmd(fmt.Sprintf("mkdir -p %s;mkdir -p %s", redisLogDir, redisDataDir))
 	fmt.Println("[redis]配置overcommit_memory...")
 	util.ExecuteCmd("echo \"vm.overcommit_memory = 1\" >> /etc/sysctl.conf;sysctl -p")
+}
+
+func startRedis() {
+	fmt.Println("[redis]启动redis...")
+	startRe, _ := util.ExecuteCmd(sys.SystemInfoObject.ServiceAction.StartRedis)
+	fmt.Println("[redis]配置redis为系统服务...")
+	enableRe, _ := util.ExecuteCmd(sys.SystemInfoObject.ServiceAction.StartRedisForever)
+	if startRe == nil && enableRe == nil {
+		util.PrintSuccessfulMsg("redis安装成功...")
+	}
 }
