f81840c7676ed27a9c7e7d8bda49ba3b9470afee
weiliang <1067599390@qq.com>
2021-10-04 16:24:57 +0800

    编写解析server列表的测试用例

--- pkg/util/strings/strings.go
+++ pkg/util/strings/strings.go
@@ -1,6 +1,9 @@
 package strings
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 // ContainAll 字符串是否含所有字符
 func ContainAll(str string, suffix ...string) bool {
@@ -10,4 +13,22 @@ func ContainAll(str string, suffix ...string) bool {
 		}
 	}
 	return false
+}
+
+func SplitIfContain(str string, contains []string) ([]string, error) {
+	count := 0
+	splitChar := ""
+	for _, v := range contains {
+		if strings.Contains(str, v) {
+			count++
+			splitChar = v
+		}
+	}
+
+	switch count {
+	case 1:
+		return strings.Split(str, splitChar), nil
+	default:
+		return nil, fmt.Errorf("%s分割符不在%s内", str, contains)
+	}
 }
