9356c406166c3b9faff3f360976cb7b782bc8670
weiliang <1067599390@qq.com>
2021-10-06 18:32:50 +0800

    添加parse测试用例

--- cmd/exec/exec.go
+++ cmd/exec/exec.go
@@ -3,13 +3,7 @@ package exec
 import (
 	// embed
 	_ "embed"
-	"fmt"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/weiliang-ms/easyctl/pkg/runner"
-	"github.com/weiliang-ms/easyctl/pkg/util/constant"
-	"k8s.io/klog"
-	"os"
 )
 
 var (
@@ -29,49 +23,6 @@ var RootCmd = &cobra.Command{
 	Short:   "执行命令指令集",
 	Example: "\neasyctl exec -c config.yaml",
 	Run: func(cmd *cobra.Command, args []string) {
-		if runErr := Exec(Entity{Cmd: cmd, Fnc: runner.Run}); runErr != nil {
-			panic(runErr)
-		}
+
 	},
-}
-
-// Entity 实体
-type Entity struct {
-	Cmd           *cobra.Command
-	Fnc           func(b []byte, logger *logrus.Logger) error
-	DefaultConfig []byte
-}
-
-// Exec 组装执行器
-func Exec(entity Entity) error {
-
-	if entity.DefaultConfig == nil {
-		entity.DefaultConfig = config
-	}
-
-	if configFile == "" {
-		klog.Infof("检测到配置文件参数为空，生成配置文件样例 -> %s", constant.ConfigFile)
-		_ = os.WriteFile(constant.ConfigFile, entity.DefaultConfig, 0666)
-		os.Exit(0)
-	}
-
-	flagset := entity.Cmd.Parent().Parent().PersistentFlags()
-	debug, err := flagset.GetBool("debug")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	b, readErr := os.ReadFile(configFile)
-	if readErr != nil {
-		klog.Fatalf("读取配置文件失败")
-	}
-
-	logger := logrus.New()
-	if debug {
-		logger.SetLevel(logrus.DebugLevel)
-	} else {
-		logger.SetLevel(logrus.InfoLevel)
-	}
-
-	return entity.Fnc(b, logger)
 }
