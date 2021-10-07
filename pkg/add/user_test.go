61019690394a6fa200ed9574fdc1e4a61e1b398a
weiliang <1067599390@qq.com>
2021-10-07 20:05:51 +0800

    添加redis部分单元测试

--- pkg/add/user_test.go
+++ pkg/add/user_test.go
@@ -17,8 +17,8 @@ new-user:
   password: "dddd"  #
   user-dir: ""      # default /home/username
 `
-	assert.Errorf(t, User(command.OperationItem{B: []byte(b), Logger: logrus.New()}),
-		"yaml: unmarshal errors:\n  line 3: cannot unmarshal !!map into []runner.ServerExternal")
+	assert.EqualError(t, User(command.OperationItem{B: []byte(b), Logger: logrus.New()}),
+		"yaml: line 3: did not find expected '-' indicator")
 
 	// 测试参数合法性
 	b = `
@@ -28,7 +28,7 @@ new-user:
   password: "dddd"  #
   user-dir: ""      # default /home/username
 `
-	assert.Errorf(t, User(command.OperationItem{B: []byte(b), Logger: logrus.New()}), "密码长度不能小于6位")
+	assert.EqualError(t, User(command.OperationItem{B: []byte(b), Logger: logrus.New()}), "密码长度不能小于6位")
 
 	// 测试模板赋值传错误参数
 	b = `
